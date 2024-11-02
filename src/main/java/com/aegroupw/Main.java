395c724b42133a7b17e7eaa4a9033c9ed902d791
Santiago Imelio <82551777+santiago-imelio@users.noreply.github.com>
2024-11-02 15:18:42 -0300

    add networkToDOT function (#2)

--- src/main/java/com/aegroupw/Main.java
+++ src/main/java/com/aegroupw/Main.java
@@ -5,17 +5,9 @@ import org.jgrapht.Graph;
 public class Main {
     public static void main(String[] args) {
         String vertexFile = "graphs/example_vertices.txt";
-        String edgesFile = "graphs/example_edges.txt"; // specify the path to your graph file
+        String edgesFile = "graphs/example_edges.txt";
         Graph<NetworkNode, NetworkEdge> graph = GraphParser.parseGraphFromFile(edgesFile, vertexFile);
 
-        // Print out the graph to verify it was constructed correctly
-        System.out.println("Vertices: " + graph.vertexSet());
-        System.out.println("Edges: " + graph.edgeSet());
-
-        for (NetworkEdge edge : graph.edgeSet()) {
-            NetworkNode source = graph.getEdgeSource(edge);
-            NetworkNode target = graph.getEdgeTarget(edge);
-            System.out.println("Edge: " + source + " -- " + target + " " + edge);
-        }
+        System.out.println(GraphParser.networkToDOT(graph));
     }
 }
