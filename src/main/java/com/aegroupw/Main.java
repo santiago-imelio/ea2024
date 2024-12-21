2e7e07a3f95b0aee5911e493d0083ded2ab99121
rde_armas <robertodearmas.de@gmail.com>
2024-12-21 16:17:39 -0300

    Add initial custom AE

--- src/main/java/com/aegroupw/Main.java
+++ src/main/java/com/aegroupw/Main.java
@@ -2,16 +2,23 @@ package com.aegroupw;
 
 import org.jgrapht.Graph;
 import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
 import org.uma.jmetal.operator.crossover.impl.SinglePointCrossover;
+import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.operator.mutation.impl.BitFlipMutation;
+import org.uma.jmetal.operator.selection.SelectionOperator;
 import org.uma.jmetal.operator.selection.impl.BinaryTournamentSelection;
 import org.uma.jmetal.util.evaluator.impl.MultiThreadedSolutionListEvaluator;
 
 import com.aegroupw.evolutionary.BinarizedNetworkSolution;
+import org.uma.jmetal.solution.binarysolution.BinarySolution;
 import com.aegroupw.evolutionary.NetworkOptimizationProblem;
 import com.aegroupw.network.NetworkEdge;
 import com.aegroupw.network.NetworkNode;
-import com.aegroupw.graphgenerator.GraphGenerator;;
+import com.aegroupw.graphgenerator.GraphGenerator;
+
+import com.aegroupw.evolutionary.CustomGeneticAlgorithm;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,36 +33,68 @@ public class Main {
                 numServers, numClients, numComponents, edgeProbability, minReliability
         );
 
-        // Print 
+        // Print graph details
         System.out.println("Generated Graph:");
         System.out.println("Nodes: " + graph.vertexSet());
         System.out.println("Edges: " + graph.edgeSet());
 
+        // Define the optimization problem
         NetworkOptimizationProblem problem = new NetworkOptimizationProblem(
             graph,
             0.8,
             1000
         );
 
-        // Define the operators
-        SinglePointCrossover cx = new SinglePointCrossover<>(0.9);
-        BitFlipMutation mutation = new BitFlipMutation<>(1.0 / problem.numberOfVariables());
-        BinaryTournamentSelection<BinarizedNetworkSolution> selection = new BinaryTournamentSelection<>();
 
-        NSGAII algo = new NSGAII(
-            problem,
-                2500,
-                    500,
-            10,
-            10,
-            cx,
-            mutation,
-            selection,
-            new MultiThreadedSolutionListEvaluator<BinarizedNetworkSolution>(2)
+        // Define the operators
+        double crossoverProbability = 0.5;
+        double mutationProbability = 0.5;
+        SelectionOperator<List<BinarySolution>, BinarySolution> selection = new BinaryTournamentSelection<>();
+        CrossoverOperator<BinarySolution> crossover = new SinglePointCrossover<BinarySolution>(
+                crossoverProbability);
+        MutationOperator<BinarySolution> mutation = new BitFlipMutation<BinarySolution>(
+                mutationProbability);
+
+        // Define evaluators
+                MultiThreadedSolutionListEvaluator<BinarySolution> multiThreadedEvaluator = new MultiThreadedSolutionListEvaluator<>(
+                2);
+
+        // ** Run the CustomGeneticAlgorithm **
+        System.out.println("\nRunning CustomGeneticAlgorithm...");
+        CustomGeneticAlgorithm customAlgo = new CustomGeneticAlgorithm(
+                problem,
+                2, // Máximo número de generaciones
+                multiThreadedEvaluator, // El evaluador de soluciones
+                selection, // El operador de selección
+                crossover, // El operador de cruce
+                mutation, // El operador de mutación
+                500 // Tamaño de la población
         );
 
-        algo.run();
+    // Execute the custom algorithm
+    customAlgo.run();
+    List<BinarySolution> customResult = customAlgo.result();
 
-        System.out.println(algo.result());
+    // Print results from CustomGeneticAlgorithm
+    System.out.println("CustomGeneticAlgorithm Result:");
+    customResult.forEach(solution -> {
+        System.out.println("Solution Objectives: " + solution.objectives()[0]);
+    });
+
+    // NSGAII algo = new NSGAII(
+    // problem,
+    // 2500,
+    // 500,
+    // 10,
+    // 10,
+    // cx,
+    // mutation,
+    // selection,
+    // new MultiThreadedSolutionListEvaluator<BinarizedNetworkSolution>(2)
+    // );
+
+    // algo.run();
+
+        // System.out.println(algo.result());
     }
 }
--- src/main/java/com/aegroupw/evolutionary/CustomGeneticAlgorithm.java
+++ src/main/java/com/aegroupw/evolutionary/CustomGeneticAlgorithm.java
@@ -1,62 +1,92 @@
 package com.aegroupw.evolutionary;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import org.uma.jmetal.algorithm.impl.AbstractGeneticAlgorithm;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.operator.selection.SelectionOperator;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
+import org.uma.jmetal.solution.binarysolution.BinarySolution;
 
-public class CustomGeneticAlgorithm extends AbstractGeneticAlgorithm {
-  public CustomGeneticAlgorithm(NetworkOptimizationProblem problem) {
+public class CustomGeneticAlgorithm
+    extends AbstractGeneticAlgorithm<BinarySolution, List<BinarySolution>> {
+
+  private final SolutionListEvaluator<BinarySolution> evaluator;
+  private int maxGenerations;
+  private int currentGeneration;
+  private int maxPopulationSize;
+
+  public CustomGeneticAlgorithm(
+      Problem<BinarySolution> problem,
+      int maxGenerations,
+      SolutionListEvaluator<BinarySolution> evaluator,
+      SelectionOperator<List<BinarySolution>, BinarySolution> selectionOperator,
+      CrossoverOperator<BinarySolution> crossoverOperator,
+      MutationOperator<BinarySolution> mutationOperator, int maxPopulationSize) {
     super(problem);
+    this.evaluator = evaluator;
+    this.maxGenerations = maxGenerations;
+    this.maxPopulationSize = maxPopulationSize;
+    this.currentGeneration = 0;
 
+    // Configura los operadores
+    this.selectionOperator = selectionOperator;
     this.crossoverOperator = crossoverOperator;
     this.mutationOperator = mutationOperator;
-    this.selectionOperator = selectionOperator;
-  }
-
-  @Override
-  public Object result() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'result'");
-  }
-
-  @Override
-  public String name() {
-    return "Custom GA";
-  }
-
-  @Override
-  public String description() {
-    return "";
+    setMaxPopulationSize(maxPopulationSize);
   }
 
   @Override
   protected void initProgress() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'initProgress'");
+    currentGeneration = 0;
   }
 
   @Override
   protected void updateProgress() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'updateProgress'");
+    currentGeneration++;
   }
 
   @Override
   protected boolean isStoppingConditionReached() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'isStoppingConditionReached'");
+    return currentGeneration >= maxGenerations;
   }
 
   @Override
-  protected List<BinarizedNetworkSolution> evaluatePopulation(List population) {
-    population = evaluator.evaluate(population, getProblem());
-
-    return population;
+  protected List<BinarySolution> evaluatePopulation(List<BinarySolution> population) {
+    return evaluator.evaluate(population, getProblem());
   }
 
   @Override
-  protected List replacement(List population, List offspringPopulation) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'replacement'");
+  protected List<BinarySolution> replacement(
+      List<BinarySolution> population,
+      List<BinarySolution> offspringPopulation) {
+
+    List<BinarySolution> mutablePopulation = new ArrayList<>(population);
+    mutablePopulation.addAll(offspringPopulation);
+    return mutablePopulation.stream()
+        .sorted((s1, s2) -> Double.compare(s1.objectives()[0], s2.objectives()[0]))
+        .limit(getMaxPopulationSize())
+        .toList();
+  }
+
+  @Override
+  public List<BinarySolution> result() {
+    return getPopulation().stream()
+        .map(solution -> (BinarySolution) solution)
+        .sorted((s1, s2) -> Double.compare(s1.objectives()[0], s2.objectives()[0]))
+        .limit(1)
+        .toList();
+  }
+
+  @Override
+  public String name() {
+    return "Custom Genetic Algorithm";
+  }
+
+  @Override
+  public String description() {
+    return "Custom implementation of a genetic algorithm for network optimization";
   }
 }
