6494facea074f0e8c19ba839f57be4b6550a84dd
Santiago Imelio <santiago.imelio@gmail.com>
2024-12-24 12:18:08 -0300

    save best solutions and fitnesses over generations, change fully connected graph reliabilities

--- src/main/java/com/aegroupw/evolutionary/CustomGeneticAlgorithm.java
+++ src/main/java/com/aegroupw/evolutionary/CustomGeneticAlgorithm.java
@@ -1,5 +1,8 @@
 package com.aegroupw.evolutionary;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
@@ -17,6 +20,10 @@ public class CustomGeneticAlgorithm
   private final SolutionListEvaluator<BinarizedNetworkSolution> evaluator;
   private int maxGenerations;
   private int currentGeneration;
+  private BinarizedNetworkSolution[] bestOfGeneration;
+  private BinarizedNetworkSolution[] bestSurvivorOfGeneration;
+  private double[] bestFitnessOfGeneration;
+  private double[] bestSurvivorFitnessOfGeneration;
 
   private NetworkOptimizationProblem problem;
 
@@ -40,6 +47,12 @@ public class CustomGeneticAlgorithm
     this.crossoverOperator = crossoverOperator;
     this.mutationOperator = mutationOperator;
     setMaxPopulationSize(maxPopulationSize);
+
+    this.bestOfGeneration = new BinarizedNetworkSolution[maxGenerations];
+    this.bestSurvivorOfGeneration = new BinarizedNetworkSolution[maxGenerations];
+
+    this.bestFitnessOfGeneration = new double[maxGenerations];
+    this.bestSurvivorFitnessOfGeneration = new double[maxGenerations];
   }
 
   @Override
@@ -59,7 +72,17 @@ public class CustomGeneticAlgorithm
 
   @Override
   protected List<BinarizedNetworkSolution> evaluatePopulation(List<BinarizedNetworkSolution> population) {
-    return evaluator.evaluate(population, getProblem());
+    List<BinarizedNetworkSolution> evaluatedPopulation = evaluator.evaluate(population, getProblem());
+
+    List<BinarizedNetworkSolution> sortedPopulation = evaluatedPopulation.stream()
+      .sorted((s1, s2) ->
+        Double.compare(problem.solutionWeightedFitness(s1), problem.solutionWeightedFitness(s2)))
+      .toList();
+
+    this.bestOfGeneration[currentGeneration] = sortedPopulation.get(0);
+    this.bestFitnessOfGeneration[currentGeneration] = problem.solutionWeightedFitness(sortedPopulation.get(0));
+
+    return evaluatedPopulation;
   }
 
   @Override
@@ -69,17 +92,20 @@ public class CustomGeneticAlgorithm
 
     List<BinarizedNetworkSolution> mutablePopulation = new ArrayList<>(population);
     mutablePopulation.addAll(offspringPopulation);
-    return mutablePopulation.stream()
+    List<BinarizedNetworkSolution> newPopulation =  mutablePopulation.stream()
         .sorted((s1, s2) ->
           Double.compare(problem.solutionWeightedFitness(s1), problem.solutionWeightedFitness(s2)))
         .limit(getMaxPopulationSize())
         .toList();
+
+    this.bestSurvivorOfGeneration[currentGeneration] = newPopulation.get(0);
+    this.bestSurvivorFitnessOfGeneration[currentGeneration] = problem.solutionWeightedFitness(newPopulation.get(0));
+    return newPopulation;
   }
 
   @Override
   protected List<BinarizedNetworkSolution> reproduction(List<BinarizedNetworkSolution> population) {
-    int numberOfParents = crossoverOperator.numberOfRequiredParents() ;
-
+    int numberOfParents = crossoverOperator.numberOfRequiredParents();
     checkNumberOfParents(population, numberOfParents);
 
     List<BinarizedNetworkSolution> offspringPopulation = new ArrayList<>(getMaxPopulationSize());
@@ -95,17 +121,82 @@ public class CustomGeneticAlgorithm
       // hack to avoid type error
       while (it.hasNext()) {
         DefaultBinarySolution s = it.next();
-        BinarizedNetworkSolution bns = new BinarizedNetworkSolution(
-          s.numberOfBitsPerVariable(), 2, problem.getEdgeProbability());
+        BinarizedNetworkSolution bns = new BinarizedNetworkSolution(s.numberOfBitsPerVariable(), 2,
+            problem.getEdgeProbability());
+
+        for (int j = 0; j < s.totalNumberOfBits(); j++) {
+          if (s.variables().get(j).get(0)) {
+            bns.variables().get(j).set(0);
+          } else {
+            bns.variables().get(j).clear(0);
+          }
+        }
+
+        bns.objectives()[0] = s.objectives()[0];
+        bns.objectives()[1] = s.objectives()[1];
+
         mutationOperator.execute(bns);
         offspringPopulation.add(bns);
       }
     }
+
     return offspringPopulation;
   }
 
   @Override
   public List<BinarizedNetworkSolution> result() {
+    try {
+      BufferedWriter outputWriter = null;
+      outputWriter = new BufferedWriter(new FileWriter("reports/best_of_generations.txt"));
+      for (int i = 0; i < bestOfGeneration.length; i++) {
+        outputWriter.write(Double.toString(bestOfGeneration[i].objectives()[0]) + " " + Double.toString(bestOfGeneration[i].objectives()[1]));
+        outputWriter.newLine();
+      }
+      outputWriter.flush();
+      outputWriter.close();
+    } catch (IOException e) {
+      System.out.println("Failed to store best solutions");
+    }
+
+    try {
+      BufferedWriter outputWriter = null;
+      outputWriter = new BufferedWriter(new FileWriter("reports/best_survivor_of_generations.txt"));
+      for (int i = 0; i < bestSurvivorOfGeneration.length; i++) {
+        outputWriter.write(Double.toString(bestSurvivorOfGeneration[i].objectives()[0]) + " " + Double.toString(bestSurvivorOfGeneration[i].objectives()[1]));
+        outputWriter.newLine();
+      }
+      outputWriter.flush();
+      outputWriter.close();
+    } catch (IOException e) {
+      System.out.println("Failed to store best solutions");
+    }
+
+    try {
+      BufferedWriter outputWriter = null;
+      outputWriter = new BufferedWriter(new FileWriter("reports/best_fitness_of_generations.txt"));
+      for (int i = 0; i < bestFitnessOfGeneration.length; i++) {
+        outputWriter.write(Double.toString(bestFitnessOfGeneration[i]) + ",");
+        outputWriter.newLine();
+      }
+      outputWriter.flush();
+      outputWriter.close();
+    } catch (IOException e) {
+      System.out.println("Failed to store best solutions");
+    }
+
+    try {
+      BufferedWriter outputWriter = null;
+      outputWriter = new BufferedWriter(new FileWriter("reports/best_survivor_fitness_of_generations.txt"));
+      for (int i = 0; i < bestSurvivorFitnessOfGeneration.length; i++) {
+        outputWriter.write(Double.toString(bestSurvivorFitnessOfGeneration[i]) + ",");
+        outputWriter.newLine();
+      }
+      outputWriter.flush();
+      outputWriter.close();
+    } catch (IOException e) {
+      System.out.println("Failed to store best solutions");
+    }
+
     return getPopulation().stream()
         .sorted((s1, s2) ->
           Double.compare(problem.solutionWeightedFitness(s1), problem.solutionWeightedFitness(s2)))
